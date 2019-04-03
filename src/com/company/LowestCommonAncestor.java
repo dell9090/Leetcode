22dbbd470733f4c2c671dcd19b8ba6cd5f12a1ee
dell9090 <liux3721@gmail.com>
2019-04-03 11:40:23 -0700

    04/03/2019

--- src/com/company/LowestCommonAncestor.java
+++ src/com/company/LowestCommonAncestor.java
@@ -9,52 +9,52 @@ package com.company;
  *     TreeNode(int x) { val = x; }
  * }
  */
-
-class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q)  {
-            return root;
-        }
+// p, q must exist in the tree
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root == null || root == p || root == q)  {
+//             return root;
+//         }
         
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
+//         TreeNode left = lowestCommonAncestor(root.left, p, q);
+//         TreeNode right = lowestCommonAncestor(root.right, p, q);
         
-        if (left != null && right != null) {// different branch
-            return root;
-        }  
+//         if (left != null && right != null) {// different branch
+//             return root;
+//         }  
         
-        return left != null ? left : right;//same branch
+//         return left != null ? left : right;//same branch
         
-    }
-}
+//     }
+// }
 
 
 // p,q may not exist
-// class Solution {
+class Solution {
     
-//     TreeNode single = new TreeNode(Integer.MAX_VALUE);
+    TreeNode single = new TreeNode(Integer.MAX_VALUE);
     
-//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-//         TreeNode tmp = helper(root, p, q);
-//         if (tmp == null || tmp == single) {
-//             return null;
-//         }
-//         return tmp;
-//     }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode tmp = helper(root, p, q);
+        if (tmp == null || tmp == single) {
+            return null;
+        }
+        return tmp;
+    }
     
-//     public TreeNode helper(TreeNode root, TreeNode p, TreeNode q) {
-//         if (root == null) {
-//             return null;
-//         }
-//         TreeNode tl = lca(root.left,  p, q);
-//         TreeNode tr = lca(root.right, p, q);
+    public TreeNode helper(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tl = lowestCommonAncestor(root.left,  p, q);
+        TreeNode tr = lowestCommonAncestor(root.right, p, q);
         
-//         if (root == p || root == q) {
-//             if (tl != null || tr != null) {
-//                 return root;
-//             }
-//             return single;
-//         }
-//         return tl == null ? tr : (tr == null ? tl : root);
-//     }
-// }
+        if (root == p || root == q) {
+            if (tl != null || tr != null) {
+                return root;
+            }
+            return single;
+        }
+        return tl == null ? tr : (tr == null ? tl : root);
+    }
+}
