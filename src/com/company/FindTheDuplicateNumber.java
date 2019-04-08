9c920d4dcc47427dcedddea07e34eeeedcf97ed5
dell9090 <liux3721@gmail.com>
2019-04-08 00:37:56 -0700

    04/08/2019

--- src/com/company/FindTheDuplicateNumber.java
+++ src/com/company/FindTheDuplicateNumber.java
@@ -1,8 +1,37 @@
 package com.company;
 
-class FindTheDuplicateNumber {
-    
-    public static void main(String[] args) {
-        // write your code here
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int low = 1;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int lowCount = count(nums, low, mid);
+            if (low == high && lowCount > 1) {
+                return low;
+            }
+            System.out.println(lowCount +" "+ low + " " + mid + " " + high);
+            if (lowCount > mid - low + 1) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
         }
+        return -1;
+    }
+    
+    // count nums in [low, high]
+    private int count(int[] nums, int low, int high) {
+        int res = 0;
+        if (low > high) {
+            return 0;
+        } 
+        for (int num : nums) {
+            if (num >= low && num <= high) {
+                res++;
+            }
+        }
+        return res;
+    }
+    
 }
