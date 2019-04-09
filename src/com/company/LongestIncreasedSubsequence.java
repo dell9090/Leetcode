a926a5acac2019236795c8bb757ae9f6d92d269b
dell9090 <liux3721@gmail.com>
2019-04-09 16:07:48 -0700

    04/09/2019

--- src/com/company/LongestIncreasedSubsequence.java
+++ src/com/company/LongestIncreasedSubsequence.java
@@ -3,25 +3,25 @@ package com.company;
 import java.util.Arrays;
 
 class Solution {
-
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length - 1;
-        int[] dp = new int[n + 1];
-        int ans;
+        int nLen = nums.length;
+        int[] dp = new int[nLen];//dp[i] : the minimum of length i + 1.  dp is an increasing array
+        int size = 0;
         Arrays.fill(dp,Integer.MAX_VALUE);
-
-        for (int num : nums) {
-            int i = Arrays.binarySearch(dp, 0, n, num);
-            if (i < 0) {
-                dp[-i - 1] = num;
-            } else {
-                dp[i] = num;
-            }
-
+        if (nums.length == 0) {
+            return 0;
         }
-        for (ans = 0; ans <= n && dp[ans] != Integer.MAX_VALUE; ans++) {}
-
-        return ans;
+        
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, nLen - 1, num);
+            if (i < 0) { // dp[i - 1] < num <= dp[i]    use num update dp[i]
+                dp[-i - 1] = num;
+                if (i == - size - 1) {  // num > all dp[]  so size + 1
+                    size++;
+                }
+            }
+        }
+        
+        return size;
     }
-
 }
